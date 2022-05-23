e1a8b1916382721d5669f678668ec5127aff1180
MamunulMazid <mamunul.mazid@gmail.com>
2022-05-23 11:42:44 +0600

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by newone on 23/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
