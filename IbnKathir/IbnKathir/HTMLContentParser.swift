b2edbf31a063df90d705d4e0d400b9211a3f24db
MamunulMazid <mamunul.mazid@gmail.com>
2022-05-29 16:17:38 +0600

    parse content class of html body

--- IbnKathir/IbnKathir/HTMLContentParser.swift
+++ IbnKathir/IbnKathir/HTMLContentParser.swift
@@ -8,25 +8,57 @@
 import Foundation
 import SwiftSoup
 
-let baseFilePath = ""
-
 class HTMLContentParser {
-    func execute() {
+    func parseConntent(fileUrl: URL) -> String {
         do {
-            var fileName = "htmml.md"
-            fileName = "1.html"
-            let fileUrl = Bundle.main.url(forResource: fileName, withExtension: "")!
             let html = try! String(contentsOf: fileUrl, encoding: .utf8)
-            
-//            let html: String = "<div><p>Lorem ipsum.</p>"
             let doc: Document = try SwiftSoup.parseBodyFragment(html)
             let body: Element? = doc.body()
-            let content = try body?.getElementsByClass("content")
-            print(content?.first())
-        } catch let Exception.Error(type, message) {
-            print(message)
+            guard let content = try body?.getElementsByClass("content") else {
+                return ""
+            }
+            let str = try content.toString()
+            return str
         } catch {
-            print("error")
+            print(error)
         }
+
+        return ""
+    }
+
+    func saveString(content: String, path: URL) throws {
+        try content.write(toFile: path.path, atomically: true, encoding: .utf8)
+    }
+
+    func traverseFiles(pathUrl: URL) {
+        let fileManager = FileManager.default
+
+        do {
+            for folder in try fileManager.contentsOfDirectory(atPath: pathUrl.path) {
+                let path = pathUrl.appendingPathComponent(folder)
+                let isDirectory = try path.resourceValues(forKeys: [.isDirectoryKey]).isDirectory ?? false
+                if isDirectory {
+                    traverseFiles(pathUrl: path)
+                } else {
+//                    print(path)
+                    do {
+                        let content = parseConntent(fileUrl: path)
+//                    print(content)
+
+                        try saveString(content: content, path: path)
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+    func execute() {
+        let directory = Bundle.main.bundleURL.appendingPathComponent("IbnKathirContents", isDirectory: true)
+        print(directory)
+        traverseFiles(pathUrl: directory)
     }
 }
