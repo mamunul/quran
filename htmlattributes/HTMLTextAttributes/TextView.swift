95d5331c639e651345b624810a3db57bdac5dfb5
MamunulMazid <mamunul.mazid@gmail.com>
2022-05-29 18:25:39 +0600

    refactor Custom UITextView

--- htmlattributes/HTMLTextAttributes/TextView.swift
+++ htmlattributes/HTMLTextAttributes/TextView.swift
@@ -8,28 +8,74 @@
 import SwiftUI
 import UIKit
 
-class CITextView: UITextView {
+struct SelectedString {
+    var range: Range<String.Index>
+    var text: String
+}
+
+class CustomUITextView: UITextView {
+    func addCustomMenu() {
+        let highlightMenuItem = UIMenuItem(title: "Highlight", action: #selector(hightlight(_:)))
+        let noteMenuItem = UIMenuItem(title: "Note", action: #selector(note(_:)))
+        UIMenuController.shared.menuItems = [highlightMenuItem, noteMenuItem]
+    }
+
+    @objc func hightlight(_ sender: Any?) {
+        let range = Range(selectedRange, in: text)!
+        let selectedString = SelectedString(range: range, text: String(text[range]))
+        print(selectedString)
+    }
+
+    @objc func note(_ sender: Any?) {
+        let range = Range(selectedRange, in: text)!
+        let selectedString = SelectedString(range: range, text: String(text[range]))
+        print(selectedString)
+    }
+
     override open func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
         if
-            action == #selector(copy(_:)) ||
-            action == #selector(select(_:)) ||
-            action == #selector(selectAll(_:))
+            action == #selector(UIResponderStandardEditActions.cut(_:)) ||
+            action == #selector(UIResponderStandardEditActions.selectAll(_:)) ||
+            action == #selector(UIResponderStandardEditActions.paste(_:)) ||
+            action == #selector(UIResponderStandardEditActions.delete(_:)) ||
+            action == Selector(("_promptForReplace:")) ||
+            action == Selector(("_transliterateChinese:")) ||
+            action == Selector(("_insertDrawing:")) ||
+            action == #selector(captureTextFromCamera(_:)) ||
+            action == Selector(("_showTextStyleOptions:")) ||
+            action == Selector(("_translate:")) ||
+            action == Selector(("_addShortcut:")) ||
+            action == Selector(("_accessibilitySpeak:")) ||
+            action == Selector(("_accessibilitySpeakLanguageSelection:")) ||
+            action == Selector(("_accessibilityPauseSpeaking:")) ||
+            action == Selector(("_share:")) ||
+            action == #selector(UIResponderStandardEditActions.makeTextWritingDirectionRightToLeft(_:)) ||
+            action == #selector(UIResponderStandardEditActions.makeTextWritingDirectionLeftToRight(_:))
+        {
+            return false
+        } else if
+            action == #selector(note(_:)) ||
+            action == #selector(hightlight(_:)) ||
+            action == #selector(UIResponderStandardEditActions.copy(_:)) ||
+            action == #selector(UIResponderStandardEditActions.select(_:)) ||
+            action == Selector(("_lookup:"))
         {
             return true
-        } else {
-            return false
         }
+
+        return true
     }
 }
 
 struct TextView: UIViewRepresentable {
     @Binding var text: NSMutableAttributedString
-
-    func makeUIView(context: Context) -> CITextView {
-        CITextView()
+    func makeUIView(context: Context) -> CustomUITextView {
+        let textview = CustomUITextView()
+        textview.addCustomMenu()
+        return textview
     }
 
-    func updateUIView(_ uiView: CITextView, context: Context) {
+    func updateUIView(_ uiView: CustomUITextView, context: Context) {
         uiView.attributedText = text
     }
 }
