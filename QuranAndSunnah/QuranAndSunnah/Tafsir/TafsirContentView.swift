8afc4973f13dc739c0a662f91f4139db6f5f08ef
MamunulMazid <mamunul.mazid@gmail.com>
2022-06-02 15:17:27 +0600

    Issue #5: add slider for fontsize

--- QuranAndSunnah/QuranAndSunnah/Tafsir/TafsirContentView.swift
+++ QuranAndSunnah/QuranAndSunnah/Tafsir/TafsirContentView.swift
@@ -10,41 +10,29 @@ import SwiftUI
 
 struct TafsirContentView: View {
     @StateObject var presenter = TafsirContentPresenter()
-    @State var scale = 1.0
-    @GestureState var scaleState = 1.0
     @State var searchStrinng: String = ""
 
-    var magnification: some Gesture {
-        MagnificationGesture()
-            .updating($scaleState) { currentState, gestureState, _ in
-                gestureState = currentState
-            }
-            .onChanged { _ in
-                scale *= scaleState
-                if scale < 1 { scale = 1 }
-                else if scale > 2 { scale = 2 }
-            }
-    }
 
     var body: some View {
         ScrollView {
             HStack {
-                TextField("TItle", text: $searchStrinng, prompt: Text("search here"))
+                TextField("Title", text: $searchStrinng, prompt: Text("search here"))
                     .keyboardType(UIKeyboardType.alphabet)
+               
                 Button {
                     presenter.recite()
                 } label: {
                     Text("recite")
                 }
-            }.padding(.horizontal)
-
-            TextView(text: $presenter.attributedContent, scale: $scale , searchString:$searchStrinng)
+            }
+            .padding(.horizontal)
+            Slider(value: $presenter.fontSize, in: presenter.fontRange, step: 1.0)
+            TextView(text: $presenter.attributedContent, searchString: $searchStrinng)
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
                 .border(.yellow)
                 .onAppear {
                     presenter.onViewAppear()
                 }
-                .gesture(magnification)
         }
     }
 }
