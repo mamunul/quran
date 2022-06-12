cd77cee79da2becb79c55c9ed4818c45b596385b
MamunulMazid <mamunul.mazid@gmail.com>
2022-06-12 12:05:47 +0600

    Issue #10: update tabitems

---
+++ QuranAndSunnah/QuranAndSunnah/AppLifeCycle/AppImage.swift
@@ -0,0 +1,12 @@
+//
+//  AppImage.swift
+//  QuranAndSunnah
+//
+//  Created by newone on 12/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+}
---
+++ QuranAndSunnah/QuranAndSunnah/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  QuranAndSunnah
+//
+//  Created by newone on 12/6/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
