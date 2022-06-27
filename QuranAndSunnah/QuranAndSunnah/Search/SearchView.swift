2cc10e4760474a3122734e40d027bf417b571907
MamunulMazid <mamunul.mazid@gmail.com>
2022-06-27 17:54:39 +0600

    Issue #21:replace text with textview in global search for better formatting

--- QuranAndSunnah/QuranAndSunnah/Search/SearchView.swift
+++ QuranAndSunnah/QuranAndSunnah/Search/SearchView.swift
@@ -12,45 +12,57 @@ struct QuranSearchView: View {
     @Binding var searchString: String
     @State var filteredAyat = [Ayah]()
     @State var allAyat = [Ayah]()
+    @AppStorage(StorageName.fontSize) var fontSize: Double = 20.0
+    var padding: CGFloat = 5
+
     var body: some View {
-        List(filteredAyat) { ayah in
-            VStack {
-                HStack {
-                    Text("\(ayah.surahNo)").padding(5)
-                    Spacer()
-                    Text("\(ayah.ayahNo)").padding(5)
+        GeometryReader { proxy in
+            List(filteredAyat) { ayah in
+                VStack {
+                    HStack {
+                        Text("\(ayah.surahNo)").padding(5)
+                        Spacer()
+                        Text("\(ayah.ayahNo)").padding(5)
+                    }.padding(.horizontal, padding)
+                    TextView(
+                        text: .constant(NSMutableAttributedString(string: ayah.text)),
+                        searchString: $searchString,
+                        fontSize: fontSize,
+                        highlights: [Highlight]()
+                    )
+                    .padding(.horizontal, padding)
+                    .frame(height: TextViewFrameCalculator.frameSize(for: ayah.text, fontSize: Int(fontSize), width: proxy.size.width - padding * 2, paragraphAlignment: .left).height)
                 }
-                Text(ayah.text)
+                .listRowInsets(EdgeInsets())
             }
-        }
-        .listStyle(PlainListStyle())
-//        .searchable(text: $searchString)
-        .onChange(of: searchString, perform: { newValue in
-            if newValue.isEmpty {
-                self.filteredAyat = allAyat
-            } else {
-                Task(priority: .utility) {
-                    let filteredAyat = allAyat.filter({ ayah in
-                        ayah.text.lowercased().contains(searchString.lowercased())
-                    })
-                    Task(priority: .userInitiated) {
-                        self.filteredAyat = filteredAyat
+            .listStyle(PlainListStyle())
+            .onChange(of: searchString, perform: { newValue in
+                if newValue.isEmpty {
+                    self.filteredAyat = allAyat
+                } else {
+                    Task(priority: .utility) {
+                        let filteredAyat = allAyat.filter({ ayah in
+                            ayah.text.lowercased().contains(searchString.lowercased())
+                        })
+                        Task(priority: .userInitiated) {
+                            self.filteredAyat = filteredAyat
+                        }
                     }
                 }
-            }
-        })
-        .onAppear {
-            Task(priority: .utility) {
-                let allAyat = presenter.getAyatTranslation()
-                var filteredAyat = allAyat
-                if !searchString.isEmpty {
-                    filteredAyat = allAyat.filter({ ayah in
-                        ayah.text.lowercased().contains(searchString.lowercased())
-                    })
-                }
-                Task(priority: .userInitiated) {
-                    self.allAyat = allAyat
-                    self.filteredAyat = filteredAyat
+            })
+            .onAppear {
+                Task(priority: .utility) {
+                    let allAyat = presenter.getAyatTranslation()
+                    var filteredAyat = allAyat
+                    if !searchString.isEmpty {
+                        filteredAyat = allAyat.filter({ ayah in
+                            ayah.text.lowercased().contains(searchString.lowercased())
+                        })
+                    }
+                    Task(priority: .userInitiated) {
+                        self.allAyat = allAyat
+                        self.filteredAyat = filteredAyat
+                    }
                 }
             }
         }
@@ -62,44 +74,56 @@ struct HadithSearchView: View {
     @Binding var searchString: String
     @State var filteredHadithList = [HadithText]()
     @State var hadithList = [HadithText]()
+    @AppStorage(StorageName.fontSize) var fontSize: Double = 20.0
+    var padding: CGFloat = 5
+
     var body: some View {
-        List(filteredHadithList) { hadith in
-            VStack {
-                HStack {
-                    Text("\(hadith.hadithNo) : \(hadith.contentId.contentId.getTitle())").padding(5)
-                    Spacer()
-                    Text("\(hadith.grade)").padding(5)
-                }
-                Text(hadith.matn)
+        GeometryReader { proxy in
+            List(filteredHadithList) { hadith in
+                VStack {
+                    HStack {
+                        Text("\(hadith.hadithNo) : \(hadith.contentId.contentId.getTitle())").padding(5)
+                        Spacer()
+                        Text("\(hadith.grade)").padding(5)
+                    }.padding(.horizontal, padding)
+                    TextView(
+                        text: .constant(NSMutableAttributedString(string: hadith.matn)),
+                        searchString: $searchString,
+                        fontSize: fontSize,
+                        highlights: [Highlight]()
+                    )
+                    .padding(.horizontal, padding)
+                    .frame(height: TextViewFrameCalculator.frameSize(for: hadith.matn, fontSize: Int(fontSize), width: proxy.size.width - padding * 2, paragraphAlignment: .left).height)
+                }.listRowInsets(EdgeInsets())
             }
-        }
-        .listStyle(PlainListStyle())
-        .onChange(of: searchString, perform: { newValue in
-            if newValue.isEmpty {
-                self.filteredHadithList = hadithList
-            } else {
-                Task(priority: .utility)  {
-                    let filteredHadithList = hadithList.filter({ hadith in
-                        hadith.matn.lowercased().contains(searchString.lowercased())
-                    })
-                    Task(priority: .userInitiated) {
-                        self.filteredHadithList = filteredHadithList
+            .listStyle(PlainListStyle())
+            .onChange(of: searchString, perform: { newValue in
+                if newValue.isEmpty {
+                    self.filteredHadithList = hadithList
+                } else {
+                    Task(priority: .utility) {
+                        let filteredHadithList = hadithList.filter({ hadith in
+                            hadith.matn.lowercased().contains(searchString.lowercased())
+                        })
+                        Task(priority: .userInitiated) {
+                            self.filteredHadithList = filteredHadithList
+                        }
                     }
                 }
-            }
-        })
-        .onAppear {
-            Task(priority: .utility) {
-                let hadithList = await presenter.getHadithList()
-                var filteredHadithList = hadithList
-                if !searchString.isEmpty {
-                    filteredHadithList = hadithList.filter({ hadith in
-                        hadith.matn.lowercased().contains(searchString.lowercased())
-                    })
-                }
-                Task(priority: .userInitiated) {
-                    self.hadithList = hadithList
-                    self.filteredHadithList = filteredHadithList
+            })
+            .onAppear {
+                Task(priority: .utility) {
+                    let hadithList = await presenter.getHadithList()
+                    var filteredHadithList = hadithList
+                    if !searchString.isEmpty {
+                        filteredHadithList = hadithList.filter({ hadith in
+                            hadith.matn.lowercased().contains(searchString.lowercased())
+                        })
+                    }
+                    Task(priority: .userInitiated) {
+                        self.hadithList = hadithList
+                        self.filteredHadithList = filteredHadithList
+                    }
                 }
             }
         }
