10988c480df69bb8e9553743a53b0a107bd30809
Mamunul Mazid <mamunul.mazid@gmail.com>
2022-07-01 12:17:25 +0600

    Issue #30: remove recursive model declaration

--- AppstoreAutomation/AppstoreAutomation/main.swift
+++ AppstoreAutomation/AppstoreAutomation/main.swift
@@ -9,19 +9,26 @@ import Foundation
 
 print("Hello, World!")
 
-let secret = "MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgVnOT7sOOOGkQMQV7miW9HQeMvnpeFgd8WcDrtTyOC3WgCgYIKoZIzj0DAQehRANCAARAi6tlmvjUoR8JkuJllx99tL3ql1r/JlDLTSQkJEmq3SCehgkflJcFqzW3S+k4At/ylzikYXkV6GeKH6unezm1"
+let secret =
+    """
+    -----BEGIN PRIVATE KEY-----
+    MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgVnOT7sOOOGkQMQV7
+    miW9HQeMvnpeFgd8WcDrtTyOC3WgCgYIKoZIzj0DAQehRANCAARAi6tlmvjUoR8J
+    kuJllx99tL3ql1r/JlDLTSQkJEmq3SCehgkflJcFqzW3S+k4At/ylzikYXkV6GeK
+    H6unezm1
+    -----END PRIVATE KEY-----
+    """
 let issuerID = "fc8ed846-b97d-47e0-b3c7-4570c073cbd6"
 let apiKey = "76CUBSK6L4"
 
-let token = AuthTokenGenerator().generateToken(secret: secret, issuerID: issuerID, apiKey: apiKey)
+let token = try? AuthTokenGenerator().generateToken(secret: secret, issuerID: issuerID, apiKey: apiKey)
 print(token)
 
 let command =
     """
     curl -v -H 'Authorization: Bearer \(token)'  "https://api.appstoreconnect.apple.com/v1/apps"
-
     """
 
 print(command)
 
-//curl -v -H 'Authorization: Bearer '  "https://api.appstoreconnect.apple.com/v1/apps"
+// curl -v -H 'Authorization: Bearer '  "https://api.appstoreconnect.apple.com/v1/apps"
