a4ea6c19c7ca5e4ca8eb650af1291a08b851abe8
Mamunul Mazid <mamunul.mazid@gmail.com>
2022-07-03 01:08:11 +0600

    Issue #30: rename classes

--- AppstoreAutomation/AppstoreAutomation/AppStoreVersion/DeleteAppStoreVersionLocalizationCommand.swift
+++ AppstoreAutomation/AppstoreAutomation/AppStoreVersion/DeleteAppStoreVersionLocalizationCommand.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class DeleteALocalizationCommand {
+class DeleteAppStoreVersionLocalizationCommand {
     struct Request {
         var appStoreVersionLocalizationId: String
     }
